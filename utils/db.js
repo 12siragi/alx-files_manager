5074c949962a0375b923cfd9e60dd90e4fe01553
12siragi <siragisaidi218@gmail.com>
2024-10-30 20:58:47 +0000

    manager

---
+++ utils/db.js
@@ -0,0 +1,73 @@
+// utils/db.js
+// utils/db.js
+import mongodb from 'mongodb';
+const { MongoClient } = mongodb;
+
+/**
+ * DBClient class that handles the connection to MongoDB
+ */
+class DBClient {
+  /**
+   * Constructor for DBClient
+   * Initializes the MongoDB client and connects to the database
+   */
+  constructor () {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}`;
+
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.client.connect()
+      .then(() => {
+        this.db = this.client.db(database);
+      })
+      .catch((err) => {
+        console.error('Failed to connect to MongoDB', err);
+      });
+  }
+
+  /**
+   * Checks if the MongoDB client is connected
+   * @returns {boolean} True if the client is connected, otherwise false
+   */
+  isAlive () {
+    return this.client && this.client.isConnected();
+  }
+
+  /**
+   * Gets the number of documents in the "users" collection
+   * @returns {Promise<number>} The number of documents in the "users" collection
+   */
+  async nbUsers () {
+    if (!this.db) {
+      return 0;
+    }
+    return this.db.collection('users').countDocuments();
+  }
+
+  /**
+ * get concern collection from database
+ * @param {*} collectionName
+ * @returns {import("mongodb").Collection} users collection
+ */
+  getCollection (collectionName) {
+    return this.db.collection(collectionName);
+  }
+
+  /**
+   * Gets the number of documents in the "files" collection
+   * @returns {Promise<number>} The number of documents in the "files" collection
+   */
+  async nbFiles () {
+    // if (!this.db) {
+    //   return 0;
+    // }
+    return this.db.collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+
+export default dbClient;
+if (process.argv[2]) console.log(dbClient);
---
+++ utils/redis.js
@@ -0,0 +1,66 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+// Redis client class
+console.log(createClient);
+class RedisClient {
+  /**
+   * constructing a new class instance
+   */
+  constructor () {
+    this.redisClient = createClient();
+    this.redisClient.on('error', (error) => {
+      console.log(error.message);
+    });
+  }
+
+  /**
+   * Check connection status of redis client
+   * @returns {boolean} - the connection status
+   */
+  isAlive () {
+    return this.redisClient.connected;
+  }
+
+  /**
+   * look for a value for a given key
+   * @param {string} key - to look for
+   * @returns {*} - value for concern key
+   */
+  async get (key) {
+    const asyncGet = promisify(this.redisClient.get).bind(this.redisClient);
+    const value = await asyncGet(key);
+    return value;
+  }
+
+  /**
+   * set the given value for the given key
+   * @param {string} key
+   * @param {*} value
+   * @param {int} duration
+   */
+
+  async set (key, value, duration) {
+    const asyncSet = promisify(this.redisClient.set).bind(this.redisClient);
+    await asyncSet(key, value, 'EX', duration);
+  }
+
+  /**
+   * deletes an entry for a given key
+   * @param {string} ke
+   */
+  async del (key) {
+    const asyncDel = promisify(this.redisClient.del).bind(this.redisClient);
+    await asyncDel(key);
+  }
+
+  /**
+   * Closes redis client connection
+   */
+  async close () {
+    this.redisClient.quit();
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
